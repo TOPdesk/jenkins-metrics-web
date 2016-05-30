ed8ca1371a951ced3fc07b5eafc6b478a82bfc17
István Rábel <thraex.aquator@gmail.com>
2016-05-30 14:37:17 +0200

    Hashbang support for bookable pages

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -2,7 +2,12 @@
   function select(event) {
     var target = $(event.target);
     var job = target.data('job');
+    selectJob(job);
+  }
+
+  function selectJob(job) {
     $.get('/' + job, function completed(result) {
+      window.location.hash = '#!' + job;
       $('#content').html(result);
     }).fail(function failed() {
       console.error('Failed to load', job);
@@ -10,10 +15,18 @@
     });
   }
 
+  function handleHashChange(event) {
+    if (window.location.hash.substr(0, 2) === '#!') {
+      event.stopPropagation();
+      selectJob(window.location.hash.substr(2));
+    }
+  }
+
   $(document).ready(function addJobClickListeners() {
     $('#list')
       .find('[data-job]')
       .click(select);
   });
-
+  $(document).ready(handleHashChange);
+  $(window).bind('hashchange', handleHashChange);
 }());
